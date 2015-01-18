7899cc65e8c9fd35536da082395fb08ca8263ff1
Amadeo Cortez <reperawin@gmail.com>
2015-01-18 02:26:03 -0700

    Bump min PHP to 5.3.8

--- install/installer.php
+++ install/installer.php
@@ -22,15 +22,14 @@ include("lib/version.php");
 include("lib/debug.php");
 include("lib/mysql.php");
 
-
 //Here goes the main thing
 
 function install()
 {
 	global $dblink, $dbserv, $dbuser, $dbpass, $dbname, $dbpref, $dberror, $abxd_version;
-	
+
 	doSanityChecks();
-	
+
 	if(file_exists("config/database.php"))
 	{
 		//TODO: Check for errors when parsing this file (It may be corrupted or wrong or whatever.
@@ -45,23 +44,23 @@ function install()
 		$dbname = $_POST['dbname'];
 		$dbpref = $_POST['dbpref'];
 	}
-	
+
 	$convert = $_POST["convert"]=="true";
 	$convertFrom = $_POST["convertFrom"];
 	$convertDbName = $_POST["convertDbName"];
 	$convertDbPrefix = $_POST["convertDbPrefix"];
-	
+
 	if(!sqlConnect())
 		installationError("Could not connect to the database. Error was: ".$dberror);
-	
+
 	$currVersion = getInstalledVersion();
-	
+
 	if($currVersion == $abxd_version)
 		installationError("The board is already installed and updated (Database version $currVersion). You don't need to run the installer!\n");
 
 	if($currVersion != -1 && $convert)
 		die("ERROR: You asked to convert a forum database, but an ABXD installation was already found in the installation DB. Converting is only possible when doing a new installation.");
-	
+
 	echo "Setting utf8_unicode_ci collation to the database...\n";
 	query("ALTER DATABASE $dbname COLLATE utf8_unicode_ci");
 
@@ -70,7 +69,7 @@ function install()
 	else
 		echo "Upgrading database from version $currVersion to $abxd_version...\n";
 	upgrade();
-	
+
 	$misc = Query("select * from {misc}");
 	if(NumRows($misc) == 0)
 		Query("INSERT INTO `{misc}` (`views`, `hotcount`, `milestone`, `maxuserstext`) VALUES (0, 30, 'Nothing yet.', 'Nobody yet.');");
@@ -80,7 +79,7 @@ function install()
 
 	if(!is_dir("config"))
 		mkdir("config");
-	
+
 	if($currVersion == -1)
 	{
 		//Stuff to do on new installation (Not upgrade)
@@ -97,7 +96,7 @@ function install()
 			writeConfigSalt();
 		}
 	}
-	
+
 	if(!file_exists("config/database.php"))
 		writeConfigDatabase();
 }
@@ -111,13 +110,10 @@ function doSanityChecks()
 {
 	$errors = array();
 	// Basic sanity tests
-	if (!function_exists('version_compare') || version_compare(PHP_VERSION, '5.0.0', '<'))
-		$errors[] = 'PHP 5.0.0 required, but you have PHP ' . PHP_VERSION . '.';
+	if (!function_exists('version_compare') || version_compare(PHP_VERSION, '5.3.8', '<'))
+		$errors[] = 'PHP 5.3.8 required, but you have PHP ' . PHP_VERSION . '.';
 	if (!function_exists('json_encode'))
-		if (version_compare(PHP_VERSION, '5.2.0', '<'))
-			$errors[] = 'As you have PHP older than PHP 5.2.0, you have to install ' .
-			            'PECL <a target="_blank" href="http://pecl.php.net/package/json">json</a> extension.';
-		elseif (version_compare(PHP_VERSION, '5.5.0', '>'))
+		if (version_compare(PHP_VERSION, '5.5.0', '>'))
 			$errors[] = 'Because of JSON licensing terms, JSON doesn\'t exist in PHP 5.5. ' .
 			            'Depending on how you installed PHP, you may need to install specific ' .
 			            'package, enable "json.so" extension in php.ini, or install ' .
@@ -143,7 +139,7 @@ function doSanityChecks()
 		echo "Your server doesn't meet the minimum requeriments for ABXD:\n";
 		foreach($errors as $error)
 			echo " - ", $error, "\n";
-		
+
 		echo "\nCan't install ABXD. Sorry!\n";
 		die();
 	}
@@ -157,13 +153,13 @@ function getInstalledVersion()
 	if(numRows(query("SHOW TABLES LIKE '{misc}'")) == 0)
 		return -1;
 
-	$row = query("SELECT * FROM {misc}");
-	
+	$row = query("SELECT version FROM {misc}");
+
 	//If no row in misc table, not installed.
 	if(numRows($row) == 0)
 		return -1;
 
-	//Otherwise return version.		
+	//Otherwise return version.
 	$row = fetch($row);
 	return $row["version"];
 }
@@ -178,7 +174,7 @@ function installationError($message)
 function writeConfigDatabase()
 {
 	global $dbserv, $dbuser, $dbpass, $dbname, $dbpref;
-	$dbcfg = @fopen("config/database.php", "w+") 
+	$dbcfg = @fopen("config/database.php", "w+")
 		or installationError(
 			"Could not open the database configuration file (config/database.php) for writing.<br>
 			 Make sure that PHP has access to this file.");
@@ -201,13 +197,13 @@ function writeConfigSalt()
 	$chct = strlen($cset) - 1;
 	while (strlen($salt) < 16)
 		$salt .= $cset[mt_rand(0, $chct)];
-		
+
 	$sltf = @fopen("config/salt.php", "w+")
 		or installationError(
 			"Could not open \"config/salt.php\" for writing. <br>
-			This has been checked for earlier, so if you see this error now, 
+			This has been checked for earlier, so if you see this error now,
 			something very strange is going on.");
-			
+
 	fwrite($sltf, "<?php \$salt = \"".$salt."\" ?>");
 	fclose($sltf);
 }
@@ -225,7 +221,7 @@ function runConverter($converter, $db, $pref)
 	global $converters;
 	if(!isValidConverter($converter))
 		die("Invalid converter!");
-		
+
 	$converter = "converter$converter";
 
 	$converter($db, $pref);
@@ -244,7 +240,7 @@ function converterIPB($db, $pref)
 	while($forum = fetch($forums))
 		if($forum["parent_id"] == -1) //Category
 			$isCat[$forum["id"]] = true;
-	
+
 	$forums = query("SELECT * FROM {$pref}forums");
 	while($forum = fetch($forums))
 	{
@@ -255,23 +251,23 @@ function converterIPB($db, $pref)
 			$parent = $forum["parent_id"];
 			if(!$isCat[$parent])
 				$parent = -$parent;
-			query("INSERT INTO {forums} (id, title, description, catid) VALUES ({0}, {1}, {2}, {3})", 
+			query("INSERT INTO {forums} (id, title, description, catid) VALUES ({0}, {1}, {2}, {3})",
 					$forum["id"], $forum["name"], $forum["description"], $parent);
 		}
 	}
-	
+
 	$insert = new ChunkedInsert("INSERT INTO {threads} (id, forum, title, sticky, closed, views) VALUES ");
-	
+
 	$threads = query("SELECT * FROM {$pref}topics");
 	while($thread = fetch($threads))
 	{
 		$closed = ($thread["state"] == "closed")?1:0;
 		$sticky = $thread["pinned"];
-		$insert->insert("({0}, {1}, {2}, {3}, {4}, {5})", 
+		$insert->insert("({0}, {1}, {2}, {3}, {4}, {5})",
 				$thread["tid"], $thread["forum_id"], $thread["title"], $sticky, $closed, $thread["views"]);
 	}
 	$insert->finish();
-	
+
 	$insert = new ChunkedInsert("INSERT INTO {posts} (id, thread, user, date, ip) VALUES ");
 	$insertText = new ChunkedInsert("INSERT INTO {posts_text} (pid, text, revision, user, date) VALUES ");
 	$posts = query("SELECT * FROM {$pref}posts");
@@ -286,15 +282,15 @@ function converterIPB($db, $pref)
 		$insert->insert("({0}, {1}, {2}, {3}, {4})", $id, 1, 1, 0, "127.0.0.1");
 		$insertText->insert("({0}, {1}, 0, {2}, {3})", $id, "HELLO WORLD SPAM LOLOLOL", 1, 0);
 	}*/
-	
+
 	$insert->finish();
 	$insertText->finish();
-	
+
 	$insert = new ChunkedInsert("INSERT INTO {users} (id, name, email, title, regdate, convertpassword, convertpasswordsalt, convertpasswordtype) VALUES ");
 	$users = query("SELECT * FROM {$pref}members");
 	while($user = fetch($users))
 	{
-		$insert->insert("({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7})", 
+		$insert->insert("({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7})",
 			$user["member_id"], $user["name"], $user["email"], $user["title"], $user["joined"], $user["members_pass_hash"], $user["members_pass_salt"], "IPB");
 	}
 	$insert->finish();
@@ -313,23 +309,23 @@ class ChunkedInsert
 		$this->data = array();
 		$this->count = 0;
 	}
-	
+
 	public function insert()
 	{
 		$args = func_get_args();
 		if (is_array($args[0])) $args = $args[0];
 		$this->data[] = parseQuery($args);
 		$this->count++;
-		
+
 		if($this->count >= 100)
 			$this->finish();
 	}
-	
+
 	public function finish()
 	{
 		if($this->count == 0)
 			return;
-		
+
 		rawQuery($this->header.implode(",",$this->data));
 		$this->data = array();
 		$this->count = 0;
